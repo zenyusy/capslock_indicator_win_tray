f352ad18fdfc8f66827f8c26b1e69a87caec2405
zenyu <zenyu@tuta.io>
2022-01-22 15:04:49 +0000

    Create cap_state.c

---
+++ cap_state.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <X11/XKBlib.h>
+
+int main() {
+    Display * d = XOpenDisplay((char*)0);
+    unsigned n;
+
+    if (d) {
+        XkbGetIndicatorState(d, XkbUseCoreKbd, &n);
+        printf("%u\n", n); /* (n & 1) ? caps_on : caps_off */
+    } /* else ERR */
+    return 0;
+}
